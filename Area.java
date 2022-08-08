e040e5fd729236ef4313a38f2ac87693c8ca7ae2
Aman Jain <83697226+amanjain5132@users.noreply.github.com>
2022-08-08 09:17:47 +0530

    Create Area.java

---
+++ Area.java
@@ -0,0 +1,61 @@
+package com.company;
+import java.util.Scanner;
+public class Area
+{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        //Area Of Circle Java Program
+//        System.out.println("enter the radius of circle");
+//        float r = in.nextFloat();
+//        float Area = (float) (3.14 * r * r);
+//        System.out.println("The Area Of Circle is : "+Area);
+
+//        //Area Of Triangle
+//        System.out.println("enter the height of the Triangle: ");
+//        float height = in.nextFloat();
+//        System.out.println("enter the breath");
+//        float breath = in.nextFloat();
+//        float area = (breath * height) / 2;
+//        System.out.println("the area of Triangle is : "+area);
+
+          //Area Of Rectangle Program
+//            System.out.println("enter the Width of the Rectangle: ");
+//            float Width = in.nextFloat();
+//            System.out.println("enter the Height");
+//            float Height = in.nextFloat();
+//            float area = (Width * Height);
+//            System.out.println("the area of Triangle is : "+area);
+
+        //Area_of_Isosceles_Triangle= (1 * b * h) / 2;
+//        System.out.println("enter the height of the Triangle: ");
+//        float height = in.nextFloat();
+//        System.out.println("enter the breath");
+//        float breath = in.nextFloat();
+//        float area = (1 * breath * height) / 2;
+//        System.out.println("Area_of_Isosceles_Triangle is : "+area);
+
+        //area of the parallelogram base*height;
+//        System.out.println("enter the height of the Triangle: ");
+//        float height = in.nextFloat();
+//        System.out.println("enter the base");
+//        float base = in.nextFloat();
+//        float area = (base * height);
+//        System.out.println("Area_of_Isosceles_Triangle is : "+area);
+
+//        // area of an equilateral triangle  ( 1.73 × a × a)/4
+//        System.out.println("enter area: ");
+//        float a = in.nextFloat();
+//        double area = ( 1.73 * a * a)/4;
+//        System.out.println("Area_of_Isosceles_Triangle is : "+area);
+
+
+        //Area Of Rhombus area=(d1*d2)/2;
+//        System.out.println("enter the Diagnal 1 of the Rhombus: ");
+//        float D1 = in.nextFloat();
+//        System.out.println("enter the Diagnal 2 of the Rhombus: ");
+//        float D2 = in.nextFloat();
+//        float area = (D1*D2)/2;
+//        System.out.println("Area Of Rhombus is : "+area);
+    }
+}
